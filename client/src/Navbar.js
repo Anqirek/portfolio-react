2e4f4644f572c1181da116d856ec0221f627f31f
Anthony Hill <hillganthony@outlook.com>
2022-02-24 18:31:17 -0500

    working on navbar

---
+++ client/src/Navbar.js
@@ -0,0 +1,16 @@
+ 
+
+function Navbar() {
+ return (
+  <nav className="nav-bar">
+   <ul>
+    <li><a className="navbar-01">About Me</a></li>
+    <li><a className="navbar-02 letter-spacing-2">Blog</a></li>
+    <li><a className="navbar-03 letter-spacing-2">Contact</a></li>
+    <li><a className="navbar-04 letter-spacing-2">Resume</a></li>
+   </ul>
+  </nav>
+ )
+}
+
+export default Navbar
