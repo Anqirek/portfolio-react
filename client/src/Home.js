728d4533c898f9ca9aca5639336e6d76468aa750
Anthony Hill <hillganthony@outlook.com>
2022-03-12 15:26:12 -0500

    centered landing page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,16 @@
-import Azure from './Images/Azure.png'
-import Bootstrap from './Images/Bootstrap.png'
-import CSS from './Images/CSS.png'
-import HTML from './Images/HTML.png'
-import Javascript from './Images/Javascript.png'
-import Postgres from './Images/Postgres.png'
-import Rails from './Images/Rails.png'
-import Reacticon from './Images/Reacticon.png'
+// import Azure from './Images/Azure.png'
+// import Bootstrap from './Images/Bootstrap.png'
+// import CSS from './Images/CSS.png'
+// import HTML from './Images/HTML.png'
+// import Javascript from './Images/Javascript.png'
+// import Postgres from './Images/Postgres.png'
+// import Rails from './Images/Rails.png'
+// import Reacticon from './Images/Reacticon.png'
 
 function Home(){
 
  return (
   <>
-   <div id='landing'>Talk to me in:</div>
    <div id='codeicons'>
      
    </div>
