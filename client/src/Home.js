d4a5057d6a5f475bf7e47ac72d82f80a5c4e66ca
Anthony Hill <hillganthony@outlook.com>
2022-03-13 21:49:05 -0400

    centered icons/added-text

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,11 +10,26 @@
 function Home(){
 
  return (
-  <>
-   <div id='codeicons'>
-     
-   </div>
-  </>
+
+ <>
+    <h3 class="titlebox">TALK TO ME IN:</h3>
+     <div class='iconbox'>
+         <p>
+             <ul class='row'>
+                 <li class='column'><img src="../Images/Azure.png" alt='Azure' />Azure</li>
+                 <li class='column'><img src="../Images/Bootstrap.png" alt='Bootstrap' />Bootstrap</li>
+                 <li class='column'><img src="../Images/css.png" alt='CSS' />CSS</li>
+                 <li class='column'><img src="../Images/html.png" alt='HTML' />HTML</li>
+                 <li class='column'><img src="../Images/Javascript.png" alt='Javascript' />Javascript</li>
+                 <li class='column'><img src="../Images/Postgres.png" alt='Postgres' />Postgres</li>
+                 <li class='column'><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg" width='144px'/>
+Rails</li>
+                 <li class='column'><img src="../Images/Reacticon.png" alt='React' />React</li>
+             </ul>
+         </p>
+     </div>
+ </>
+    
  )
 }
 
